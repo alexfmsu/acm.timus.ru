c7989f378b87c8770e0e19b4a1beaef40d5bda26
alexfmsu <alexfmsu@mail.ru>
2017-07-28 15:21:41 +0300

    urgrade

---
+++ 1293/1293.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+using namespace std;
+
+int main(){
+    int N;
+    
+    int A, B;
+    
+    cin >> N >> A >> B;
+    
+    cout << (N * A * B) * 2 << endl;
+    
+    return 0;
+}
